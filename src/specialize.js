4e674dfc5c78f4962d5926c63a9e89450667180e
Zhi Wei Gan <zhiwei_gan@hotmail.com>
2020-09-27 12:31:22 +0800

    Addressing Issue #5

--- src/specialize.js
+++ src/specialize.js
@@ -8,57 +8,54 @@ const rea = require('./reaper-objects')
  * @property {ReaData[]} [contents] optional contents
  */
 function specialize (obj) {
-  if (!(obj instanceof ReaperBase)) return obj
+  if (!(obj instanceof ReaperBase)) return obj;
 
   switch (obj.token) {
     case 'NOTES':
-      obj = new rea.ReaperNotes(obj)
-      break
+      obj = new rea.ReaperNotes(obj);
+      break;
     case 'TRACK':
-      obj = new rea.ReaperTrack(obj)
-      break
+      obj = new rea.ReaperTrack(obj);
+      break;
     case 'REAPER_PROJECT':
-      obj = new rea.ReaperProject(obj)
-      break
+      obj = new rea.ReaperProject(obj);
+      break;
     case 'ITEM':
-      for (const i in obj.contents) {
-        if (obj.contents[i].token === 'SOURCE') {
-          if (obj.contents[i].params[0] === 'MIDI') {
-            obj = new rea.ReaperMidiItem(obj)
-          } else if (obj.contents[i].params[0] === 'WAVE') {
-            obj = new rea.ReaperAudioItem(obj)
-          }
-        }
+      obj = new rea.ReaperItem(obj);
+      break;
+    case 'SOURCE':
+      switch(obj.params[0]){
+        case 'WAVE':
+          obj = new rea.ReaperAudioSource(obj);
+          break;
+        case 'MIDI':
+          obj = new rea.ReaperMidiSource(obj);
+          break;
       }
-      if (obj.params[0] === 'WAVE') {
-        obj = new rea.ReaperAudioItem(obj)
-      } else if (obj.params[0] === 'MIDI') {
-        obj = new rea.ReaperMidiItem(obj)
-      }
-      break
+      break;
     case 'VST':
-      obj = new rea.ReaperVst(obj)
-      break
+      obj = new rea.ReaperVst(obj);
+      break;
     case 'FXCHAIN':
-      obj = new rea.ReaperFXChain(obj)
-      break
+      obj = new rea.ReaperFXChain(obj);
+      break;
     case 'VOLENV2':
-      obj = new rea.ReaperVolumeAutomation(obj)
-      break
+      obj = new rea.ReaperVolumeAutomation(obj);
+      break;
     case 'PANENV2':
-      obj = new rea.ReaperPanAutomation(obj)
-      break
+      obj = new rea.ReaperPanAutomation(obj);
+      break;
     case 'WIDTHENV2':
-      obj = new rea.ReaperWidthAutomation(obj)
-      break
+      obj = new rea.ReaperWidthAutomation(obj);
+      break;
     default:
-      break
+      break;
   }
 
   for (const i in obj.contents) {
-    obj.contents[i] = specialize(obj.contents[i])
+    obj.contents[i] = specialize(obj.contents[i]);
   }
-  return obj
+  return obj;
 }
 
-module.exports = specialize
+module.exports = specialize;
